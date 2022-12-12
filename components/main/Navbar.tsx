dec31382bb14bdcc4d581e421fc1d255b6cb530c
addliam <liamyostin2004@gmail.com>
2022-12-12 16:25:31 -0500

    initial s etup

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="debug bg-gray-300 md:w-[180px] h-screen"></div>;
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        dark: "#000000",
+        lightBlue: "#2081E2",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
